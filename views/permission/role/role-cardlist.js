033720d1c8eb43fdcd55107320b266557fbc0c58
Preshine <peixia_6766@163.com>
2018-12-06 18:45:18 +0800

    ..

--- views/permission/role/role-cardlist.js
+++ views/permission/role/role-cardlist.js
@@ -41,14 +41,109 @@ $(function () {
                 text: '编辑',
                 handler: function (record) {
                     console.log('edit' + JSON.stringify(record));
+                    $('input[name=roleId]').val(record.id);
+                    $('input[name=name]').val(record.name);
+                    $('#description').val(record.description);
+                    $('#addOrEditModal').modal();
                 }
             }, {
                 text: '分配资源',
                 handler: function (record) {
                     console.log('change res' + JSON.stringify(record));
+                    $('#authModal').modal();
                 }
             }],
             target: '#cardlist'
         })
     })
 })
+
+//定义中文消息
+var cnmsg = {
+    required: '必填字段',
+    remote: '请修正该字段',
+    email: '请输入正确格式的电子邮件',
+    url: '请输入合法的网址',
+    date: '请输入合法的日期',
+    dateISO: '请输入合法的日期 (ISO).',
+    number: '请输入合法的数字',
+    digits: '只能输入整数',
+    creditcard: '请输入合法的信用卡号',
+    equalTo: '请再次输入相同的值',
+    accept: '请输入拥有合法后缀名的字符串',
+    maxlength: jQuery.validator.format('请输入一个长度最多是 {0} 的字符串'),
+    minlength: jQuery.validator.format('请输入一个长度最少是 {0} 的字符串'),
+    rangelength: jQuery.validator.format('请输入一个长度介于 {0} 和 {1} 之间的字符串'),
+    range: jQuery.validator.format('请输入一个介于 {0} 和 {1} 之间的值'),
+    max: jQuery.validator.format('请输入一个最大为 {0} 的值'),
+    min: jQuery.validator.format('请输入一个最小为 {0} 的值')
+};
+
+jQuery.extend(jQuery.validator.messages, cnmsg);
+
+var e = $("#resourcesTree"),
+    r = $(".alert-danger", e),
+    i = $(".alert-success", e);
+var validator = e.validate({
+    errorElement: "span",
+    errorClass: "help-block help-block-error",
+    focusInvalid: !1,
+    ignore: "",
+    rules: {
+        name: {
+            minlength: 2, required: true
+        },
+        path: {
+            required: true
+        },
+        resType: {
+            required: true
+        }
+    },
+    invalidHandler: function (e, t) {
+        i.hide(), r.show();
+        // App.scrollTo(r, -200)
+    },
+    highlight: function (e) {
+        $(e).closest(".form-group").addClass("has-error")
+    },
+    unhighlight: function (e) {
+        $(e).closest(".form-group").removeClass("has-error")
+    },
+    success: function (e) {
+        e.closest(".form-group").removeClass("has-error")
+    },
+    submitHandler: function (e) {
+        i.show(), r.hide()
+        var formData = {};
+        formData.roleId = $('input[name=roleId]').val();
+        formData.name = $('input[name=name]').val();
+        formData.description = $('#description').val();
+        formData.PSESSIONID = sessionStorage.getItem('p_token');
+        console.log(formData);
+        $('#addOrEditModal').modal('hide');
+        $.ajax({
+            url: 'http://127.0.0.1:8082/preshine/api/resources/addOrEdit1',
+            type: 'post',
+            data: formData,
+            // contentType: 'application/json; charset=utf-8',
+            contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+            dataType: 'json',
+            success: function (res) {
+                if (res.success == true) {
+                    window.location.reload();
+                } else {
+
+                }
+            }
+        })
+    }
+})
+
+function showAdd() {
+    validator.resetForm();
+    i.hide(), r.hide()
+    $('input[name=roleId]').val('');
+    $('input[name=name]').val('');
+    $('#description').val('');
+}
