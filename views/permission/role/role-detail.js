b4805916bd8d8ea73e3e2230ad7303e83be16fed
Preshine <peixia_6766@163.com>
2018-12-14 17:50:03 +0800

    ..

--- views/permission/role/role-detail.js
+++ views/permission/role/role-detail.js
@@ -1,45 +1,91 @@
-function cardList(props) {
-    var data = props.data;
-    var actions = props.actions;
-    var target = props.target;
-    var cardClick = props.cardClick;
+$(function () {
 
-    var html = '<div style="display: flex;flex-wrap: wrap;">';
+    //1.初始化Table
+    var oTable = TableInit();
+    oTable.Init();
 
-    data.forEach((d, di) => {
-        var str = '<div class="card" style="width:200px;margin: 10px 10px;">'
-            + '<img class="card-img-top" src="http://static.runoob.com/images/mix/img_avatar.png" alt="Card image" style="width:100%">'
-            + '<div class="card-body">'
-            + '<h4 class="card-title"><a href="javascript:void(0)" class="name_' + di + '" >' + d.name + '</a></h4>'
-            + '<p class="card-text">' + d.description + '</p>';
+    //2.初始化Button的点击事件
+    var oButtonInit = ButtonInit();
+    oButtonInit.Init();
 
-        $(document).on('click', target + ' .name_' + di, function () {
-            if (cardClick) {
-                cardClick(d);
-            }
+});
+
+
+var TableInit = function (roleId) {
+    var oTableInit = {};
+    //初始化Table
+    oTableInit.Init = function () {
+        $('#tb_departments').bootstrapTable({
+            url: 'http://127.0.0.1:8082/preshine/api/role/getUsersByrole1',         //请求后台的URL（*）
+            method: 'get',                      //请求方式（*）
+            contentType:'application/x-www-form-urlencoded; charset=UTF-8',
+            toolbar: '#toolbar',                //工具按钮用哪个容器
+            striped: true,                      //是否显示行间隔色
+            cache: false,                       //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
+            pagination: true,                   //是否显示分页（*）
+            sortable: false,                     //是否启用排序
+            sortOrder: "asc",                   //排序方式
+            queryParams: oTableInit.queryParams,//传递参数（*）
+            sidePagination: "server",           //分页方式：client客户端分页，server服务端分页（*）
+            pageNumber: 1,                       //初始化加载第一页，默认第一页
+            pageSize: 10,                       //每页的记录行数（*）
+            // pageList: [10, 20, 30],        //可供选择的每页的行数（*）
+            search: false,                       //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
+            strictSearch: true,
+            showColumns: false,                  //是否显示所有的列
+            showRefresh: false,                  //是否显示刷新按钮
+            minimumCountColumns: 2,             //最少允许的列数
+            clickToSelect: true,                //是否启用点击选中行
+            height: 500,                        //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
+            uniqueId: "id",                     //每一行的唯一标识，一般为主键列
+            showToggle: false,                    //是否显示详细视图和列表视图的切换按钮
+            cardView: false,                    //是否显示详细视图
+            detailView: false,                   //是否显示父子表
+            columns: [{
+                checkbox: true
+            }, {
+                field: 'id',
+                title: 'ID'
+            }, {
+                field: 'userName',
+                title: '用户名'
+            }, {
+                field: 'mobile',
+                title: '手机号'
+            }, {
+                field: 'sex',
+                title: '性别'
+            },]
         });
+    };
 
-        str += '<div>'
-        actions.forEach((action, ai) => {
-            str += '<button class="btn btn-primary card_' + di + ai + '">' + (action.renderer ? action.renderer(d) : action.text) + '</button>';
-            //这样绑定事件会让后来加入的dom节点也会添加上点击事件
-            $(document).on('click', target + ' .card_' + di + ai, function () {
-                console.log('action', action);
-                action.handler(d);
-            });
-        })
+    //得到查询的参数
+    oTableInit.queryParams = function (params) {
+        console.log(params)
+        var temp = {   //这里的键的名字和控制器的变量名必须一直，这边改动，控制器也需要改成一样的
+            pageSize: params.limit,   //页面大小
+            current: (params.offset / params.limit) + 1,  //页码
+            roleId: 1,
+            search: params.search
+            // departmentname: $("#txt_search_departmentname").val(),
+            // statu: $("#txt_search_statu").val()
+        };
+        return temp;
+    };
+    return oTableInit;
+};
 
-        str += '</div>';
-        str += '</div>';
-        str += '</div>';
 
-        html += str;
-    });
+var ButtonInit = function () {
+    var oInit = {};
+    var postdata = {};
 
-    html += "</div>";
+    oInit.Init = function () {
+        //初始化页面上面的按钮事件
+    };
 
-    $(target).html(html);
-}
+    return oInit;
+};
 
 $(function () {
     $.get('http://127.0.0.1:8082/preshine/api/role/list?PSESSIONID=' + sessionStorage.getItem('p_token'), function (res) {
